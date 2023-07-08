a39f249b954ee28e01fcae6da934ebee9dcb61d5
Dmitry Karpekin <dkarpekin@twelvedevs.com>
2023-07-08 12:29:50 +0300

    fix after review

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static("./dist"));
-app.use("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/index.html"));
+const publicPath = path.join(__dirname, "dist");
+
+app.use(express.static(`${__dirname}/dist`));
+
+app.get("/*", (request, response) => {
+  const indexPath = path.join(publicPath, "index.html");
+
+  if (fs.existsSync(indexPath)) {
+    response.status(200).sendFile(indexPath);
+  } else {
+    response.status(200).send("File not found");
+  }
+
+  response.sendFile(path.join(publicPath, "index.html"));
 });
 
 app.listen(PORT, () => {
--- src/pages/chats/chats.ts
+++ src/pages/chats/chats.ts
@@ -3,7 +3,7 @@ import arrChat from "./modules/chat/chat";
 import listChats from "./modules/listChats/listChats";
 import userChat from "./modules/userChat/userChat";
 
-const chats = document.createElement("main");
+const chats = document.createElement("section");
 chats.classList.add(styles.chats_wrapper);
 chats.append(listChats);
 
