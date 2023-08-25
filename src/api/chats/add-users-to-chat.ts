68a3d5dd7428618da0850e72a930a9da3c3117cd
Dmitry Karpekin <dkarpekin@twelvedevs.com>
2023-08-26 01:24:18 +0300

    fix new line

--- src/api/chats/add-users-to-chat.ts
+++ src/api/chats/add-users-to-chat.ts
@@ -3,7 +3,7 @@ import HTTPTransport from "../../services/HTTPTransport";
 const addUsersToChatAPIInstance = new HTTPTransport();
 
 class AddUsersToChatAPI {
-  request(data: { users: number[], chatId: number }) {
+  request(data: { users: number[]; chatId: number }) {
     return addUsersToChatAPIInstance.put("/chats/users", { data });
   }
 }
--- src/api/chats/delete-users-from-chat.ts
+++ src/api/chats/delete-users-from-chat.ts
@@ -3,7 +3,7 @@ import HTTPTransport from "../../services/HTTPTransport";
 const deleteUsersFromChatAPIInstance = new HTTPTransport();
 
 class DeleteUsersFromChatAPI {
-  request(data: { users: number[], chatId: number }) {
+  request(data: { users: number[]; chatId: number }) {
     return deleteUsersFromChatAPIInstance.delete("/chats/users", { data });
   }
 }
